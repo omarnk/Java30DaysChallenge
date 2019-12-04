7c14c6126502d649f1565b22e260d449cb3069d2
Omar Khaled <omar.khaled.en92@gmail.com>
2019-12-04 17:38:58 +0200

    Update Solution.java

--- Solution-DataTypes-Day01/src/main/java/com/solution/solution/datatypes/day01/Solution.java
+++ Solution-DataTypes-Day01/src/main/java/com/solution/solution/datatypes/day01/Solution.java
@@ -1,6 +1,11 @@
 package com.solution.solution.datatypes.day01;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Solution {
 	
@@ -11,22 +16,29 @@ public class Solution {
 		
         Scanner scan = new Scanner(System.in);
         /* Declare second integer, double, and String variables. */
-        int a;
-        double b;
-        String c;
-        //int a = scan.nextInt();
-        //double b = scan.nextDouble();
-        //String c = scan.nextLine();
+        //int a;
+        //double b;
+        String c = null;
+        int a = scan.nextInt();
+        double b = scan.nextDouble();
+        //String c = scan.next();
+        BufferedReader ob = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            c = ob.readLine();
+        } catch (IOException ex) {
+            Logger.getLogger(Solution.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       
+        
         /* Read and save an integer, double, and String to your variables.*/
         // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
-       a = 12;
-       b = 4.0;
-       c = "is the best place to learn and practice coding!";
+       //a = 12;
+       //b = 4.0;
+       //c = "is the best place to learn and practice coding!";
         /* Print the sum of both integer variables on a new line. */
         int intSum = i + a;
         double o = b + d;
-        //double doubleSum = int(d + b);
-        String stSum = s + c;
+        String stSum = s.concat(c);
         /* Print the sum of the double variables on a new line. */
         System.out.println(intSum);
         System.out.println(o);
